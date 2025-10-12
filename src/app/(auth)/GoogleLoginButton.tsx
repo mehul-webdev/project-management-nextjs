a04588fe40533967fbbba947c4976861a505017f
mehulwebdev96@gmail.com <mehulchaudhary330@gmail.com>
2025-10-12 19:38:46 +0530

    added log for the google login testing

--- src/app/(auth)/GoogleLoginButton.tsx
+++ src/app/(auth)/GoogleLoginButton.tsx
@@ -2,6 +2,9 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 
 const GoogleLoginButton = ({ authState }: { authState: string }) => {
+  console.log(
+    `here url hit is: ${process.env.NEXT_PUBLIC_BASE_LOCAL_API_URL}/api/auth/google`
+  );
   return (
     <Button
       variant="outline"
