13a2368a37f3796ea3eb3242ac2ab15647513d76
mehulwebdev96@gmail.com <mehulchaudhary330@gmail.com>
2025-10-19 19:22:38 +0530

    updated axios lib

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_BASE_API_URL ||
-    process.env.NEXT_PUBLIC_BASE_LOCAL_API_URL,
+  baseURL: "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
@@ -12,12 +10,12 @@ const api = axios.create({
 
 // Request interceptor
 // Request interceptor
-// api.interceptors.request.use(
-//   (config) => {
-//     const fullURL = (config.baseURL || "") + (config.url || "");
-//     console.log("Request URL:", fullURL);
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
+api.interceptors.request.use(
+  (config) => {
+    const fullURL = (config.baseURL || "") + (config.url || "");
+    console.log("Request URL:", fullURL);
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 export default api;
